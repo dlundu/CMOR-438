16598091d03c1df5bd8e53282f860b5847458e8c
Denise Lundu <dgl3@rice.edu>
2026-05-02 13:27:40 -0400

    Update __init__.py

---
+++ src/rice_ml/unsupervised_learning/__init__.py
@@ -0,0 +1,17 @@
+# --- Dimensionality Reduction ---
+from .svd import SVD
+from .pca import PCA
+ 
+# --- Clustering ---
+from .kmeans import KMeans
+from .dbscan import DBSCAN
+from .community_detection import LabelPropagation
+ 
+__all__ = [
+    "SVD",
+    "PCA",
+    "KMeans",
+    "DBSCAN",
+    "LabelPropagation",
+]
+ 
